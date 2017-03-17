bd22f962510cd152f3e6019597131eeba96bd6ed
Greg Garvey <ggarvey@stanford.edu>
2017-03-17 13:59:04 -0700

    using isotope rather than css columns

---
+++ js/emphasis_isotope.js
@@ -0,0 +1,35 @@
+(function ($) {
+  Drupal.behaviors.emphasis = {
+    attach: function (context, settings) {
+      // Code to be run on page load, and
+      // on ajax load added here
+      $('.view-neuro-research-emphasis .view-content').imagesLoaded(function(){
+        $('.view-neuro-research-emphasis .view-content').isotope({
+          // options
+          itemSelector: '.view-neuro-research-emphasis .views-row',
+          masonry: {
+            // use element for option
+            //columnWidth: 400,
+            gutter: 20
+          },
+          itemPositionDataEnabled: true
+        });
+      });
+      $( document ).ajaxComplete(function() {
+        $('.view-neuro-research-emphasis .view-content').imagesLoaded(function(){
+          $('.view-neuro-research-emphasis .view-content').isotope({
+            // options
+            itemSelector: '.view-neuro-research-emphasis .views-row',
+            masonry: {
+              // use element for option
+              //columnWidth: 400,
+              gutter: 10
+            },
+            //itemPositionDataEnabled: true
+          });
+        });
+        $('.view-neuro-research-emphasis .view-content').isotope('reloadItems');
+      });
+    }
+  };
+}(jQuery));
