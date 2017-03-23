6b06ccb343054f5b47fa056277bdb3b717846054
Greg Garvey <ggarvey@stanford.edu>
2017-03-22 21:57:37 -0700

    markup mods

--- templates/views-view-fields--neuro-research-emphasis.tpl.php
+++ templates/views-view-fields--neuro-research-emphasis.tpl.php
@@ -23,7 +23,6 @@
  *
  * @ingroup views_templates
  */
-//dpm($row);
 global $user;
 $node = node_load($row->nid);
 if (node_access("update", $node, $user) === TRUE) {
@@ -47,9 +46,8 @@ switch ($row->node_type) {
     }
     $output .= '<div class="well">';
     $output .= '<div>'; //undocumented div
-    $output .= '<div class="type-event">Event</div>';
-    $output .= '<div class="event-date-long descriptor">' . drupal_render($row->field_field_stanford_event_datetime[0]['rendered']) . ' ' . drupal_render($row->field_field_stanford_event_datetime_1[0]['rendered']) . '</div>';
-    $output .= '<div class="event-title normal-link"><h3>' . $row->node_title . '</h3></div>';
+    $output .= '<div class="event-date-long descriptor">Event - ' . drupal_render($row->field_field_stanford_event_datetime[0]['rendered']) . ' ' . drupal_render($row->field_field_stanford_event_datetime_1[0]['rendered']) . '</div>';
+    $output .= '<div class="event-title normal-link"><h3><a href="' . $node_path . '">' . $row->node_title . '</a></h3></div>';
     $output .= '</div>'; //end undocumented div
     if ($node_acces) {
       $output .= '<div class="edit-link">' . l(t("Edit"), "node/" . $row->nid . '/edit', array("query" => array("destination" => $node_path))) . '</div>';
@@ -60,12 +58,13 @@ switch ($row->node_type) {
   case 'stanford_news_item':
     $output = '<div class="masonry-news">';
     if (isset($row->field_field_s_image_info[0])) {
-      $output .= '<div>' . drupal_render($row->field_field_s_image_info[0]['rendered']) . '</div>';
+      unset($row->field_field_s_image_info[0]['rendered']['entity']['field_collection_item'][$row->field_field_s_image_info[0]['raw']['value']]['field_s_image_caption']);
+      $output .= '<div><a href="' . $node_path . '">' . drupal_render($row->field_field_s_image_info[0]['rendered']) . '</a></div>';
     }
     $output .= '<div class="well">';
-    $output .= '<div class="type-event">News - ' . drupal_render($row->field_field_s_news_date[0]['rendered'])  . '</div>';
+    $output .= '<div class="type-event descriptor">News - ' . drupal_render($row->field_field_s_news_date[0]['rendered'])  . '</div>';
     $output .= '<div class="descriptor">' . drupal_render($row->field_field_s_news_source[0]['rendered']) . '</div>';
-    $output .= '<div class="normal-link"><h3>' . $row->node_title . '</h3></div>';
+    $output .= '<div class="normal-link"><h3><a href="' . $node_path . '">' . $row->node_title . '</a></h3></div>';
     if ($node_acces) {
       $output .= '<div class="edit-link">' . l(t("Edit"), "node/" . $row->nid . '/edit', array("query" => array("destination" => $node_path))) . '</div>';
     }
@@ -76,7 +75,7 @@ switch ($row->node_type) {
     if ($row->field_field_s_announce_tweet[0]['raw']['value'] == 1) {
       //This is a tweet
       $output = '<div class="masonry-tweet well">';
-      $output .= '<div class="descriptor">Twitter ' . drupal_render($row->field_field_s_announcement_date[0]['rendered']) . '</div>';
+      $output .= '<div class="descriptor">Twitter - ' . drupal_render($row->field_field_s_announcement_date[0]['rendered']) . '</div>';
       $output .= '<div>' . drupal_render($row->field_body[0]['rendered']) . '</div>';
       if ($node_acces) {
         $output .= '<div class="edit-link">' . l(t("Edit"), "node/" . $row->nid . '/edit', array("query" => array("destination" => $node_path))) . '</div>';
@@ -86,11 +85,11 @@ switch ($row->node_type) {
     else {
       $output = '<div class="masonry-announcement">';
       if (isset($row->field_field_s_image_info[0])) {
-        $output .= '<div>' . drupal_render($row->field_field_s_image_info[0]['rendered']) . '</div>';
+        $output .= '<div><a href="' . $row->field_field_s_announcement_source[0]['raw']['url'] . '">' . drupal_render($row->field_field_s_image_info[0]['rendered']) . '</a></div>';
       }
       $output .= '<div class="well">';
-      $output .= '<div class="descriptor">Announcement ' . drupal_render($row->field_field_s_announcement_date[0]['rendered']) . '</div>';
-      $output .= '<div class="normal-link"><h3>' . $row->node_title . '</h3></div>';
+      $output .= '<div class="descriptor">Announcement - ' . drupal_render($row->field_field_s_announcement_date[0]['rendered']) . '</div>';
+      $output .= '<div class="normal-link"><h3><a href="' . $row->field_field_s_announcement_source[0]['raw']['url'] . '">' . $row->node_title . '</a></h3></div>';
       $output .= '<div>' . drupal_render($row->field_body[0]['rendered']) . '</div>';
       if ($node_acces) {
         $output .= '<div class="edit-link">' . l(t("Edit"), "node/" . $row->nid . '/edit', array("query" => array("destination" => $node_path))) . '</div>';
@@ -102,10 +101,11 @@ switch ($row->node_type) {
   case 'stanford_funded_research':
     $output = '<div class="masonry-research">';
     if (isset($row->field_field_s_image_info[0])) {
-      $output .= '<div>' . drupal_render($row->field_field_s_image_info[0]['rendered']) . '</div>';
+      unset($row->field_field_s_image_info[0]['rendered']['entity']['field_collection_item'][$row->field_field_s_image_info[0]['raw']['value']]['field_s_image_caption']);
+      $output .= '<div><a href="' . $node_path . '">' . drupal_render($row->field_field_s_image_info[0]['rendered']) . '</a></div>';
     }
     $output .= '<div class="well">';
-    $output .= '<div class="descriptor">Funded Research ' . drupal_render($row->field_field_s_fund_research_type[0]['rendered']) . '</div>';
+    $output .= '<div class="descriptor">Funded Research - ' . drupal_render($row->field_field_s_fund_research_type[0]['rendered']) . '</div>';
     $output .= '<div class="normal-link"><h3>' . $row->node_title . '</h3></div>';
     if ($node_acces) {
       $output .= '<div class="edit-link">' . l(t("Edit"), "node/" . $row->nid . '/edit', array("query" => array("destination" => $node_path))) . '</div>';
